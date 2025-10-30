1b674915ea89d947ecd33f75cb1791fa25a0ece2
opilane <opilane@HP-CZC2349HT0>
2025-10-30 09:56:04 +0200

    Add project files.

---
+++ PythonApplication2.py
@@ -0,0 +1,11 @@
+﻿print("ÜL 1 Jaanes:")
+number = int(input("kirjuta number (0-9): "))
+
+a = 0
+while a < number:
+    print("")
+    print("(\_/)")
+    print("(o o)")
+    print("/ | \*")
+    print("")
+    a = a + 1
---
+++ module1.py
@@ -0,0 +1,7 @@
+a = int(input("Sisesta L: "))
+b = 0
+c = 0
+while c <= a:
+    b = b + c
+    c = c + 1
+print("Summa =", b)
