c84049f6194c63524c0660ac6ba43ac0c260fabd
opilane <opilane@HP-CZC2349HT0>
2025-10-30 11:14:11 +0200

    123

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,11 +1,49 @@
-﻿print("ÜL 1 Jaanes:")
-number = int(input("kirjuta number (0-9): "))
+﻿import random
 
-a = 0
-while a < number:
-    print("")
-    print("(\_/)")
-    print("(o o)")
-    print("/ | \*")
-    print("")
-    a = a + 1
+print("ül 1 jänes:")
+num = int(input("kirjuta number (0-9): "))
+
+if 0 < num < 10:
+    rida1 = "  (\\_/)  "
+    rida2 = "  (o o)  "
+    rida3 = "  / | \\* "
+
+    print(rida1 * num)
+    print(rida2 * num)
+    print(rida3 * num)
+else:
+    print("Ei saa sisestada arvu, mis on väiksem kui 1 ja suurem kui 9.")
+
+print("\nül 2 summa:")
+L = int(input("kirjuta arv L: "))  
+
+summa = 0  
+a = 0  
+
+while a <= L:  
+    summa = summa + a  
+    a = a + 1  
+
+print("summa on", summa)  
+
+print("\nül 3 arva ära arv (0-100):")
+n = random.randint(0, 100)  
+k = 0  
+ok = False 
+
+while k < 10:
+    a = int(input("pakkumine: "))  
+    if a == n:
+        print("õige number oli", n) 
+        ok = True
+        break
+    elif a < n:
+        print("number on suurem")  
+    else:
+        print("number on väiksem")  
+    k = k + 1
+
+if ok == False:
+    print("vale, õige number oli", n)  
+
+print("")
